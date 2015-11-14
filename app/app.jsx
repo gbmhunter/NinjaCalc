b6601e85f0a63a00c845e8094e31495fda7f735e
Geoffrey Hunter <gbmhunter@gmail.com>
2015-11-14 23:43:39 +1300

    Added CSS styling to grid layout.

--- app/app.jsx
+++ app/app.jsx
@@ -19,6 +19,7 @@ var Select = require('react-select');
 import Dropdown from 'react-dropdown';
 import { Tooltip, OverlayTrigger, Popover } from 'react-bootstrap';
 var PureRenderMixin = require('react-addons-pure-render-mixin');
+var _ = require('lodash');
 
 var ReactRadioGroup = require('react-radio-group');
 
@@ -221,6 +222,110 @@ var Calculator = React.createClass({
 	}
 })
 
+var NoDraggingLayout = React.createClass({
+  mixins: [PureRenderMixin],
+
+  propTypes: {
+    onLayoutChange: React.PropTypes.func.isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      className: "layout",
+      isDraggable: false,
+      isResizable: false,
+      items: 50,
+      cols: 12,
+      rowHeight: 30
+    };
+  },
+
+  getInitialState() {
+    var layout = this.generateLayout();
+    return {
+      layout: layout
+    };
+  },
+
+  generateDOM() {
+    return _.map(_.range(this.props.items), function(i) {
+      return (<div key={i}><span className="text">{i}</span></div>);
+    });
+  },
+
+  generateLayout() {
+    var p = this.props;
+    return _.map(new Array(p.items), function(item, i) {
+      var y = _.result(p, 'y') || Math.ceil(Math.random() * 4) + 1;
+      return {x: i * 2 % 12, y: Math.floor(i / 6) * y, w: 2, h: y, i: i};
+    });
+  },
+
+  onLayoutChange: function(layout) {
+    //this.props.onLayoutChange(layout);
+  },
+
+  render() {
+    return (
+      <ReactGridLayout layout={this.state.layout} onLayoutChange={this.onLayoutChange}
+          {...this.props}>
+        {this.generateDOM()}
+      </ReactGridLayout>
+    );
+  }
+});
+
+var BasicLayout = React.createClass({
+  mixins: [PureRenderMixin],
+
+  propTypes: {
+    onLayoutChange: React.PropTypes.func.isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      className: "layout",
+      items: 20,
+      rowHeight: 30,
+      cols: 12
+    };
+  },
+
+  getInitialState() {
+    var layout = this.generateLayout();
+    return {
+      layout: layout
+    };
+  },
+
+  generateDOM() {
+    return _.map(_.range(this.props.items), function(i) {
+      return (<div key={i}><span className="text">{i}</span></div>);
+    });
+  },
+
+  generateLayout() {
+    var p = this.props;
+    return _.map(new Array(p.items), function(item, i) {
+      var y = _.result(p, 'y') || Math.ceil(Math.random() * 4) + 1;
+      return {x: i * 2 % 12, y: Math.floor(i / 6) * y, w: 2, h: y, i: i};
+    });
+  },
+
+  onLayoutChange: function(layout) {
+    //this.props.onLayoutChange(layout);
+  },
+
+  render() {
+    return (
+      <ReactGridLayout layout={this.state.layout} onLayoutChange={this.onLayoutChange}
+          {...this.props}>
+        {this.generateDOM()}
+      </ReactGridLayout>
+    );
+  }
+});
+
 //class App extends React.Component {
 var App = React.createClass({
 
@@ -241,11 +346,7 @@ var App = React.createClass({
 		return (
 			<div>	
 				{/*<AbsoluteGrid items={sampleItems} />*/}
-				<ReactGridLayout className="layout" cols={12} rowHeight={30}>
-			      <div key={1} _grid={{x: 0, y: 0, w: 1, h: 2}}>1</div>
-			      <div key={2} _grid={{x: 1, y: 0, w: 1, h: 2}}>2</div>
-			      <div key={3} _grid={{x: 2, y: 0, w: 1, h: 2}}>3</div>
-			    </ReactGridLayout>
+				<BasicLayout />
 				{/* Let's create a table for every calculator in array */
 					this.props.state.get('calculators').map(function(el) {
 						return <Calculator key={el.get('id')} data={el} dispatch={that.props.dispatch} />
