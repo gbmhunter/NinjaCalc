9f37be36802295369b723a55d08d2d2772ca3808
Geoffrey Hunter <gbmhunter@gmail.com>
2016-01-20 21:38:30 +1300

    Started moving "Info" code into custom WPF user control.

---
+++ src/Core/View/CalcInfo.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace NinjaCalc.Core.View {
+    /// <summary>
+    /// Interaction logic for CalcInfo.xaml
+    /// </summary>
+    public partial class CalcInfo : UserControl {
+        public CalcInfo() {
+            InitializeComponent();
+        }
+    }
+
+    public class MultiplyConverter : IMultiValueConverter {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            double result = 1.0;
+            for (int i = 0; i < values.Length; i++) {
+                if (values[i] is double)
+                    result *= (double)values[i];
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new Exception("Not implemented");
+        }
+    }
+}
