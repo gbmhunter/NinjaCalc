00841080a754bebfe434ca070533a92220197ee5
Geoffrey Hunter <gbmhunter@gmail.com>
2016-03-07 23:09:09 +1300

    First unit test now tries to create calculator instance to test, but fails.

--- test/Calculators/Electronics/Basic/StandardResistanceFinder/StandardResistanceFinderModelTest.java
+++ test/Calculators/Electronics/Basic/StandardResistanceFinder/StandardResistanceFinderModelTest.java
@@ -11,6 +11,13 @@ public class StandardResistanceFinderModelTest {
     @Test
     public void firstTest() {
         assertTrue(true);
+
+        StandardResistanceFinderModel standardResistanceFinderModel = new StandardResistanceFinderModel();
+
+        standardResistanceFinderModel.desiredResistance.setRawVal(10);
+        standardResistanceFinderModel.eSeries.setRawVal("E96");
+        assert 10.0 == standardResistanceFinderModel.actualResistance.getRawVal();
+
     }
 
 }
