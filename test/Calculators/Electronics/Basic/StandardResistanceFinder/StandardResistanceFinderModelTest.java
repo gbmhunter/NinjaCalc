878b078ed5f726967aff96617509d2698bf216ba
Geoffrey Hunter <gbmhunter@gmail.com>
2016-03-07 22:49:01 +1300

    Added example unit test.

---
+++ test/Calculators/Electronics/Basic/StandardResistanceFinder/StandardResistanceFinderModelTest.java
@@ -0,0 +1,16 @@
+package Calculators.Electronics.Basic.StandardResistanceFinder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gbmhunter on 2016-03-07.
+ */
+public class StandardResistanceFinderModelTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+
+}
