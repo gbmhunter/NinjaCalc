edd847daeb3c66231a179ee0a4c11db32723f16c
Geoffrey Hunter <gbmhunter@gmail.com>
2016-03-07 23:27:07 +1300

    First unit test which actually creates a JavaFX scene and then tests a calculator instance is working.

--- test/Calculators/Electronics/Basic/StandardResistanceFinder/StandardResistanceFinderModelTest.java
+++ test/Calculators/Electronics/Basic/StandardResistanceFinder/StandardResistanceFinderModelTest.java
@@ -1,23 +1,43 @@
 package Calculators.Electronics.Basic.StandardResistanceFinder;
 
+import Utility.StandardResistanceFinder;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by gbmhunter on 2016-03-07.
- */
-public class StandardResistanceFinderModelTest {
-    @Test
-    public void firstTest() {
-        assertTrue(true);
+public class StandardResistanceFinderModelTest extends ApplicationTest {
 
-        StandardResistanceFinderModel standardResistanceFinderModel = new StandardResistanceFinderModel();
+    StandardResistanceFinderModel standardResistanceFinderModel;
 
-        standardResistanceFinderModel.desiredResistance.setRawVal(10);
-        standardResistanceFinderModel.eSeries.setRawVal("E96");
-        assert 10.0 == standardResistanceFinderModel.actualResistance.getRawVal();
-
+    @Override
+    public void start(Stage stage) {
+        standardResistanceFinderModel =  new StandardResistanceFinderModel();
+        Scene scene = new Scene(standardResistanceFinderModel.view, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 
+    @Test
+    public void e12SeriesTest10k0() {
+        // given:
+        //rightClickOn("#desktop").moveTo("New").clickOn("Text Document");
+        //write("myTextfile.txt").push(ENTER);
+
+        // when:
+        //drag(".file").dropTo("#trash-can");
+
+        // then:
+        //verifyThat("#desktop", hasChildren(0, ".file"));
+        //verifyThat("#desktop", hasChildren(0, ".file"));
+        System.out.println("Setting desiredResistance.setRawVal().");
+        standardResistanceFinderModel.desiredResistance.setRawVal(10.0);
+        System.out.println("Value = " + standardResistanceFinderModel.eSeries.getRawVal());
+
+        assert 10.0 == standardResistanceFinderModel.actualResistance.getRawVal();
+
+
+    }
 }
