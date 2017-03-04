9939d2b6e7d48f9f4b56551f12504141dc95633f
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-04 13:35:25 -0500

    Reverting to commit 965812f9d6c7201a8b4ceefeb3108052cbe80094.

--- test/unit/specs/StandardResistanceFinder.spec.js
+++ test/unit/specs/StandardResistanceFinder.spec.js
@@ -3,7 +3,7 @@ import StandardResistanceFinder from 'src/misc/StandardResistanceFinder/Standard
 describe('Users factory', function () {
   var standardResistanceFinder = new StandardResistanceFinder()
 
-  it('E6 series test', function () {
+  it('has a dummy spec to test 2 + 2', function () {
     // An intentionally failing test. No code within expect() will never equal 4.
     var res = standardResistanceFinder.find(101, standardResistanceFinder.eSeriesOptions.E6, standardResistanceFinder.searchMethods.CLOSEST)
     expect(res).to.equal(100)
