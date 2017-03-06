02e6b353cb844442f1bc43f5d56e4c007a6b3e2d
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-05 21:01:38 -0500

    Fixed failing unit tests.

--- test/unit/specs/CrcCatalogue.spec.js
+++ test/unit/specs/CrcCatalogue.spec.js
@@ -9,7 +9,7 @@ describe('CrcCatalogue object tests.', function () {
     expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).crcPolynomial.toJSNumber()).to.equal(0x31)
   })
   it('CRC starting value can be retrieved correctly.', function () {
-    expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).startingValue.toJSNumber).to.equal(0x00)
+    expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).startingValue.toJSNumber()).to.equal(0x00)
   })
   it('reflect data variable is set correctly.', function () {
     expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).reflectData).to.equal(true)
@@ -18,7 +18,7 @@ describe('CrcCatalogue object tests.', function () {
     expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).reflectRemainder).to.equal(true)
   })
   it('Final XOR value is set correctly.', function () {
-    expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).finalXorValue.toJSNumber).to.equal(0x00)
+    expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).finalXorValue.toJSNumber()).to.equal(0x00)
   })
   it('Check value is set correctly.', function () {
     expect(crcCatalogue.get(crcIds.CRC_8_MAXIM).checkValue.toJSNumber()).to.equal(0xA1)
@@ -27,4 +27,8 @@ describe('CrcCatalogue object tests.', function () {
 
 describe('CrcCatalogue CRC check value tests.', function () {
   console.log('to be implemented')
+  for (var [key, value] of crcCatalogue.presetCrcAlgorithms) {
+    console.log(key + ' = ' + value)
+    
+  }
 })
