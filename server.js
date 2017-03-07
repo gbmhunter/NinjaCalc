208b0e8167ac4057d6fb87d16d9e06000a78f7f6
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-07 18:39:13 -0500

    Updated "server.js" with will emit a periodic 5-min keep-alive ping to the Heroku app.

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ app.use(serveStatic(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+port);
+
+// Create a periodic HTTP GET request which will
+// ping the Heroku app and keep it alive
+var http = require("http");
+setInterval(function() {
+  http.get('http://http://ninja-calc.herokuapp.com');
+}, 300000); // every 5 minutes (300000)
