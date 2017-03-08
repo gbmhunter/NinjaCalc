d4ae703855d42e00d2d148e3983ea68d6fb60370
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-08 12:21:19 -0500

    Added more console.log() statements to "server.js".

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+port);
+console.log('Server started on port "' + port + '".');
 
 // Create a periodic HTTP GET request which will
 // ping the Heroku app and keep it alive
+console.log('Setting up 5min ping...')
 var http = require("http");
 setInterval(function() {
   console.log('Pinging http://ninja-calc.mbedded.ninja/.');
   http.get('http://ninja-calc.mbedded.ninja/');
 }, 300000); // every 5 minutes (300000)
+
+console.log('server.js finished.')
