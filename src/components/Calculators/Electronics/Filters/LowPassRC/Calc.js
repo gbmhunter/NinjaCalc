0a39c3239ec4aa91ccb6aca2bc8d2794a3829016
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-10 17:38:52 -0500

    Tidied up the visual appearance of the calculator selection overlay.

--- src/components/Calculators/Electronics/Filters/LowPassRC/Calc.js
+++ src/components/Calculators/Electronics/Filters/LowPassRC/Calc.js
@@ -2,7 +2,7 @@ import MainView from './MainView'
 
 export default {
   displayName: 'Low Pass RC',
-  description: 'The low-pass RC filter is probably the simplist and most used electronic filter. Great for input signal filtering and adding to the output of a PWM signal to make a cheap DAC.',
+  description: 'The low-pass RC filter is probably the simplist and most used electronic filter. Great for input signal filtering.',
   imagePath: require('./grid-icon.png'),
   category: [ 'Electronics', 'Filters' ],
   tags: [ 'rc', 'filters', 'filtering', 'low-pass', 'adc', 'signal', 'conditioning', 'processing' ],
