250fc715807a2971b97eb84db301c72bc5178cf3
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-16 18:57:43 -0400

    Added description to the "Capacitor Charge" calculator and gave it an improved "grid-icon" image.

--- src/components/Calculators/Electronics/Basic/CapacitorCharge/Calc.js
+++ src/components/Calculators/Electronics/Basic/CapacitorCharge/Calc.js
@@ -2,9 +2,9 @@ import MainView from './MainView'
 
 export const capacitorChargeCalculator = {
   displayName: 'Capacitor Charge (Q=CV)',
-  description: '.',
+  description: 'Calculate either the charge, capacitance or voltage across a capacitor using Q = CV.',
   imagePath: require('./grid-icon.png'),
   category: [ 'Electronics', 'Basic' ],
-  tags: [ 'capacitor', 'capacitance', 'charge', 'voltage', 'farad' ],
+  tags: [ 'capacitor', 'capacitance', 'charge', 'voltage', 'farad', 'coulomb', 'electron' ],
   mainView: MainView
 }
