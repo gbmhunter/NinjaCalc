884bb1448ecd104351a39d51501bfc3d8cd01489
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-17 11:41:40 -0400

    Added "deploy.sh".

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# When copying across the dist/ folder, rsync will
+# also delete any folders/files in destination that are
+# not present in the source
+echo "Coping across dist/ folder..."
+rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress --delete -avz --ignore-existing --recursive dist/ -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc
+
+# This time, do not delete files in destination that are
+# not present in source!
+echo "Copying across .htaccess file..."
+rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress -avz server-config/.htaccess -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc
