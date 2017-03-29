fd8f81f63984ddb6b6ec97a35f47d93ffcdc8590
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-29 15:14:17 -0700

    Added "/" to end of destination path in deploy.sh in the hope that it will prevent duplicate folders from being created.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # Also need to chmod so that files are 0644 and folders 0755
 # (otherwise people won't be able to access website)
 echo "Coping across dist/ folder..."
-rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress --delete -avz --ignore-existing --recursive dist/ -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc
+rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress --delete -avz --ignore-existing --recursive dist/ -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc/
 # This time, do not delete files in destination that are
 # not present in source!
 echo "Copying across .htaccess file..."
