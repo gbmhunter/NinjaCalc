3dddf3943001fd5fe8e7508887671a64d4c62203
Geoffrey Hunter <gbmhunter@gmail.com>
2017-03-29 13:53:53 -0700

    Removed empty lines from deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # When copying across the dist/ folder, rsync will
 # also delete any folders/files in destination that are
 # not present in the source
@@ -7,7 +6,6 @@
 # (otherwise people won't be able to access website)
 echo "Coping across dist/ folder..."
 rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress --delete -avz --ignore-existing --recursive dist/ -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc
-
 # This time, do not delete files in destination that are
 # not present in source!
 echo "Copying across .htaccess file..."
