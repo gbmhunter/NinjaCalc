92e9f7574665e6173a9d3d3b145453b73123cf25
Geoffrey Hunter <gbmhunter@gmail.com>
2018-04-25 12:12:57 -0700

    Fixed bug where deployment did not work correctly without first deleting everything under ninja-calc dir on server, closes #160. Fixed by removing '--ignore-existing' flag in call to rsync.

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,14 @@
 # (otherwise people won't be able to access website)
 # "/" IS required at end of destination path, otherwise rsync creates duplicate folders
 # on remote server (I don't even know how that is possible)
+# -a is archive
+# -v: Verbose mode
+# -z: Compress data when sending
+# --delete: rsync will delete files on the receiving side that arn't on the sending size
+# --recursive: Copy files/directories recursively
 echo "Coping across dist/ folder..."
-rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress --delete -avz --ignore-existing --recursive dist/ -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc
+rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r --progress --delete -avz --recursive dist/ -e ssh gbmhunter@160.153.162.18:/home/gbmhunter/public_html/ninja-calc
+
 # This time, do not delete files in destination that are
 # not present in source!
 echo "Copying across .htaccess file..."
