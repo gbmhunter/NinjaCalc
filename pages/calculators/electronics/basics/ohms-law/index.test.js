edbc6b5a3e602704b26e0eb3c2623f2e938c219a
Geoffrey Hunter <gbmhunter@gmail.com>
2020-09-23 21:36:03 +1200

    Functional test now modifies inputs.

--- pages/calculators/electronics/basics/ohms-law/index.test.js
+++ pages/calculators/electronics/basics/ohms-law/index.test.js
@@ -3,10 +3,18 @@ import { mount } from 'enzyme';
 import CalcOhmsLaw from './index'
 
 describe('calc-ohms-law-tests', () => {
-  it('default values should be set', () => {
-    const wrapper = mount(<CalcOhmsLaw />)    
-    expect((wrapper.find('input[name="voltage"]')).props().value).toBe("12")
-    expect((wrapper.find('input[name="current"]')).props().value).toBe("1")
-    expect((wrapper.find('input[name="resistance"]')).props().value).toBe("12.00")
+  it('changing values works', () => {
+    const wrapper = mount(<CalcOhmsLaw />)
+    let voltageInput = wrapper.find('input[name="voltage"]')
+    let currentInput = wrapper.find('input[name="current"]')
+    let resistanceInput = wrapper.find('input[name="resistance"]')
+
+    expect(voltageInput.props().value).toBe("12")
+    expect(currentInput.props().value).toBe("1")
+    expect(resistanceInput.props().value).toBe("12.00")
+
+    voltageInput.at(0).instance().value = '6'
+    voltageInput.simulate('change')    
+    expect(wrapper.find('input[name="resistance"]').props().value).toBe("6.000")
   });
 });
