3c72d479326d7f801597f71154c024ef088c4207
Geoffrey Hunter <gbmhunter@gmail.com>
2020-09-27 21:49:58 +1300

    Attempt to fix regex.

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = withPlugins([
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       // Note: we provide webpack above so you should not `require` it
       // Perform customizations to webpack config
-      config.plugins.push(new webpack.IgnorePlugin(/\.test.js$/))
+      config.plugins.push(new webpack.IgnorePlugin(/\.*test.js$/))
   
       // Important: return the modified config
       return config
