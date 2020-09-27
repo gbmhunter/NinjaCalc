77a3dec40c3984ca1e98c9ffbb3560c2cdb1a34b
Geoffrey Hunter <geoff@biolumic.com>
2020-09-28 09:36:35 +1300

    Improved the unit tests for the wire guage calculator.

--- __tests__/calc-wire-guage.test.js
+++ __tests__/calc-wire-guage.test.js
@@ -5,11 +5,26 @@ import { getValue, setValue } from 'utils/testing-utils'
 import CalcWireGuage from 'pages/calculators/electronics/cabling/wire-guage-calculator'
 
 describe('calc-ohms-law-tests', () => {
+  it('is mountable', () => {
+    const wrapper = mount(<CalcWireGuage />)
+  })
+
   it('changing values works', () => {
     const wrapper = mount(<CalcWireGuage />)
 
+    // Check default values
     expect(getValue(wrapper, 'voltageDc_V')).toBe('12')
-    setValue(wrapper, 'voltageDc_V', 5)
-    expect(getValue(wrapper, 'guage_awg')).toBe(7)
+
+    // Now set all values explicitly
+    setValue(wrapper, 'voltageDc_V', '5')
+    setValue(wrapper, 'voltageDrop_perc', '2')
+    setValue(wrapper, 'cableLength_m', '5')
+    setValue(wrapper, 'current_A', '3')
+    setValue(wrapper, 'conductorMaterial', 'Copper')
+    expect(getValue(wrapper, 'guage_awg')).toBe(13)
+
+    // Change the conductor material
+    setValue(wrapper, 'conductorMaterial', 'Aluminium')
+    expect(getValue(wrapper, 'guage_awg')).toBe(11)
   })
 })
--- utils/testing-utils.js
+++ utils/testing-utils.js
@@ -1,9 +1,13 @@
 export function getValue(wrapper, calcVarName) {
-  return wrapper.find('input[name="' + calcVarName + '"]').props().value
+  // Have to specify a parent .value class here because the value and unit input elements
+  // both have the same name
+  return wrapper.find('.value input[name="' + calcVarName + '"]').props().value
 }
 
-export function setValue(wrapper, calcVarName, value) {
-  let component = wrapper.find('input[name="' + calcVarName + '"]')
+export function setValue(wrapper, calcVarName, value) {  
+  // Have to specify a parent .value class here because the value and unit input elements
+  // both have the same name
+  let component = wrapper.find('.value [name="' + calcVarName + '"]')  
   component.at(0).instance().value = value
   component.simulate('change')    
 }
